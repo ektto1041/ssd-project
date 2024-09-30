3101cf8917277177215dcc91297cf6f7a5d25710
itcuna0617 <itcuna0617@gmail.com>
2024-09-30 11:38:48 +0900

    temp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import ssd.SSDImpl;
+
 public class Main {
     public static void main(String[] args) {
+        SSDImpl ssd = new SSDImpl();
 
+        ssd.write(2, "0x12345678");
     }
+
 }
--- src/main/java/ssd/SSDImpl.java
+++ src/main/java/ssd/SSDImpl.java
@@ -1,9 +1,33 @@
 package ssd;
 
+import fileio.FileIO;
+import java.util.Arrays;
+
 public class SSDImpl implements SSD {
+
+    FileIO io;
+    int[] lba = new int[100];
+
+    public SSDImpl(){
+        Arrays.fill(this.lba, -1);
+    };
+
+    public SSDImpl(FileIO io){
+        this.io = io;
+        Arrays.fill(this.lba, -1);
+    };
+
     @Override
     public void write(int index, String value) {
+        this.lba[index] = Integer.decode(value);
+        String tot = "";
 
+        for(int i = 0; i < lba.length; i++){
+            tot += Integer.toHexString(lba[i]) + (i != 99 ? "|" : "");
+        }
+
+        System.out.println(tot);
+//        io.write("nand.txt", tot);
     }
 
     @Override
